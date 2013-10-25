9999adb8c56180d6ae1c9aeb2b484da113475a1a
Thomas Boerger <tboerger@tbpro.de>
2013-10-25 10:17:20 +0200

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name "dropbox"
+maintainer "Thomas Boerger"
+maintainer_email "tboerger@tbpro.de"
+license "Apache 2.0"
+description "Installs/Configures dropbox"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version "0.0.1"
+recipe "dropbox", "Installs/Configures dropbox"
+
+supports "suse"
